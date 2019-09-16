e7c9ea769cbb644f525e664621362c1eeb13d1d7
tmcintire <tmc1618@gmail.com>
2019-09-15 23:50:14 -0500

    file changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App "/>
+    <input v-model="name" />
+    {{name}}
   </div>
 </template>
 
@@ -13,6 +15,9 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   components: {
     HelloWorld,
   },
+  data: () => ({
+    name: 'Bammer bammer ding dong',
+  }),
 })
 export default class Home extends Vue {}
 </script>
