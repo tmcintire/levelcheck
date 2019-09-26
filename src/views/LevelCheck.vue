f10a56f2751f23b1778e0054fa7cfb2977d829c9
Tim <tmc1618@gmail.com>
2019-09-26 16:50:40 -0500

    add vuetify

---
+++ src/views/LevelCheck.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="event" class="registrations">
+    <h1>Level check for {{ event.name }}</h1>
+  </div>
+  <div v-else>
+    <h3>Please select an event first</h3>
+    <EventSelector />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { store } from '../store';
+import { IParticipant } from '../data/interfaces';
+import EventSelector from '@/components/EventSelector.vue'; // @ is an alias to /src;
+import { mapState } from 'vuex';
+import { getNextId } from '../helpers';
+
+@Component({
+  components: {
+    EventSelector,
+  },
+  computed: mapState(['event']),
+})
+export default class LevelCheck extends Vue {}
+</script>
---
+++ src/views/LevelCheckTutorial.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="registrations">
+    <h1>Level check user tutorial</h1>
+    <b-button @click="skipTutorial">Skip tutorial</b-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { store } from '../store';
+import { IParticipant } from '../data/interfaces';
+import EventSelector from '@/components/EventSelector.vue'; // @ is an alias to /src;
+import { mapState } from 'vuex';
+import { getNextId } from '../helpers';
+import { skipTutorial } from '../data/api';
+import { router } from '../router';
+
+@Component
+export default class LevelCheckTutorial extends Vue {
+  public skipTutorial() {
+    const needsLCTutorial = store.getters.user.levelCheckTutorial;
+    console.log(store);
+
+    skipTutorial('levelCheckTutorial', false).then(() => {
+      router.push('levelcheck');
+    });
+  }
+}
+</script>
